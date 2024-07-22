3c58e2ff7fd4ca5f2b1703f6b24c753a60560b55
Your Name <you@example.com>
2024-07-22 23:26:39 +0530

    update file in which i add recursive solution

--- Day1/InsertionSort.java
+++ Day1/InsertionSort.java
@@ -4,7 +4,12 @@ public class InsertionSort {
     public static void main(String[] args) {
         //array declaration
         int arr[]={10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
-        //this for loop is iterate all value
+        System.out.println("Nornal method \n");
+        insertion(arr);
+        System.out.println("\n Using Recursion to solve this question :\n");
+        usingRecursion(arr,1);
+    }
+    static void insertion(int arr[]){
         for(int i=1;i<arr.length;i++){
             int j=i;
             //asign value j in i
@@ -16,9 +21,24 @@ public class InsertionSort {
                 j--;
             }
         }
-        System.out.println("Insertion sort" );
         for(int i:arr){
             System.out.print(i+"  ");
         }
+    }
+    static void usingRecursion(int arr[],int i){
+        if(i==arr.length){
+            for(int j:arr){
+                System.out.print(j+"  ");
+            }
+            return;
+        }
+        int j=i;
+        while(j>0 && arr[j]<arr[j-1]){
+            int temp=arr[j];
+            arr[j]=arr[j-1];
+            arr[j-1]=temp;
+            j--;
+        }
+        usingRecursion(arr,i+1);
     }
 }

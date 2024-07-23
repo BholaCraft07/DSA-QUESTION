0cbb97ac41193d1d901e9c421b12ddb74c2ece35
Your Name <you@example.com>
2024-07-23 19:18:24 +0530

    Find largest Element in array

---
+++ Day2/LargestElement_InArray.java
@@ -0,0 +1,14 @@
+public class LargestElement_InArray {
+    public static void main(String[] args) {
+        // first we assume array first value is largest element
+        // int arr[]={1, 8, 7, 56, 90};
+        int arr[]={5, 5, 5, 5};
+        int max=arr[0];
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>max){
+                max=arr[i];
+            }
+        }
+        System.out.println("Largest Element in Array :"+max);
+    }
+}
